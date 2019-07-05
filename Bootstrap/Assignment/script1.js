9358d78b4edde01f3002022ba2c65613ec880a23
Pro4tech <pritesh04naik@gmail.com>
2019-07-05 10:21:27 +0530

    Assignment Page Edits

--- Bootstrap/Assignment/script1.js
+++ Bootstrap/Assignment/script1.js
@@ -1,24 +1,16 @@
 var user = {
     email: ['abc@gmail.com', 'p4t@gmail.com'],
     pass: ['1234', 'abcd'],
-    check: function () {
-        var i, sum = 0;
-        for (i = 0; i < this.email.length; i++) {
-            if (document.getElementById('email').value == this.email[i] && document.getElementById('pass').value == this.pass[i]) {
-                window.location.replace("Home.html");
-            }
-
-        }
-    },
 }
 
 function check() {
-    var i;
+    var i,find=0;
     for (i = 0; i < user.email.length; i++) {
         if (document.getElementById('email').value == user.email[i] && document.getElementById('pass').value == user.pass[i]) {
             console.log('Match Found');
-            window.location.replace("Home.html");
+            top.location="Home.html";
+            find=1;
         }
     }
-    console.log('Match Not Found');
+    if(find==0){alert("Incorrect Account Credentials.Try Again");}
 }
