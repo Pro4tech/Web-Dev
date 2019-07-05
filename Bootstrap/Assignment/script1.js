dd6893804a009b236ac9aab01e0bf885f4bf452c
Pro4tech <pritesh04naik@gmail.com>
2019-07-05 10:38:09 +0530

    New Edits

--- Bootstrap/Assignment/script1.js
+++ Bootstrap/Assignment/script1.js
@@ -4,13 +4,15 @@ var user = {
 }
 
 function check() {
-    var i,find=0;
+    var i, find = 0;
     for (i = 0; i < user.email.length; i++) {
         if (document.getElementById('email').value == user.email[i] && document.getElementById('pass').value == user.pass[i]) {
             console.log('Match Found');
-            top.location="Home.html";
-            find=1;
+            top.location = "Home.html";
+            find = 1;
         }
     }
-    if(find==0){alert("Incorrect Account Credentials.Try Again");}
+    if (find == 0) {
+        alert("Incorrect Account Credentials.Try Again");
+    }
 }
